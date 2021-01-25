bc4bbc94e42ae22a8f25e1dce72d0f2458534d23
mehild <mhild@mcad.edu>
2021-01-24 22:56:50 -0500

    commit color
    stroke color update

--- week 1/snow/snowman.js
+++ week 1/snow/snowman.js
@@ -1,12 +1,45 @@
 let grid = undefined;
 function setup() {
     createCanvas(1000, 800);
-    background("#ccc");
-    grid = loadImage("images/100px_grid.png");
+    background("#DED7F3");
 }
+
 function draw() {
-    background(grid);
-    // snowman legs
-    fill("#f1f1f1");
-    stroke("100, 250, 100");
+
+    // bottom
+    stroke("#926DFD");
+    strokeWeight(10);
+    ellipse(500, 600, 300);
+    // middle
+    stroke("#926DFD");
+    strokeWeight(10);
+    ellipse(500, 400, 200); 
+    // head
+    stroke("#926DFD");
+    strokeWeight(10);
+    ellipse(500, 275, 100);
+    
+    // eyes
+    stroke("#000000");
+    strokeWeight(8);
+    point(480, 275);
+    point(510, 275);
+
+    //nose
+    stroke("#FF6400 ");
+     
+    // lil middle
+    stroke("#926DFD");
+    strokeWeight(5);
+    ellipse(250, 700, 150); 
+    // lil head
+    stroke("#926DFD");
+    strokeWeight(5);
+    ellipse(250, 600, 90);
+    // lil eyes
+    stroke("#000000");
+    strokeWeight(5);
+    point(240, 585);
+    point(260, 585);
+   
 }
