8318b2741545820c17abdf970f48c7924830c6c6
mehild <mhild@mcad.edu>
2021-02-04 22:24:00 -0500

    function

--- week 3/exercises/pattern/pattern.js
+++ week 3/exercises/pattern/pattern.js
@@ -2,3 +2,32 @@ function setup() {
     createCanvas(600, 600);
     noLoop();
 }
+function createTile() {
+    translate(0, 0);
+    fill('#334d00');
+    rect(0, 0, 200, 200);
+    stroke('#558000');
+    strokeWeight('10');
+    line(0, 0, 200, 200);
+    line(200, 0, 0, 200);
+    noStroke();
+    fill('#223300');
+    circle(100, 50, 50);
+    circle(50, 100, 50);
+    circle(150, 100, 50);
+    circle(100, 150, 50);
+    noFill();
+    stroke('#b3ff1a');
+    strokeWeight('1');
+    line(0, 0, 200, 200);
+    line(200, 0, 0, 200);
+    stroke('#408000');
+    strokeWeight('4');
+    circle(50, 100, 100);
+    circle(150, 100, 100);
+    circle(100, 50, 100);
+    circle(100, 150, 100);
+}
+function draw(){
+    createTile();
+}
