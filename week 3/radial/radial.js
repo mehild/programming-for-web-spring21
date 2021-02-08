f3b2a6160e685a23995ffbc3d5d5b480f3ee7735
mehild <mhild@mcad.edu>
2021-02-08 01:25:22 -0500

    rotate
    play

--- week 3/radial/radial.js
+++ week 3/radial/radial.js
@@ -1,12 +1,29 @@
+let rotateBy= 4;
 function setup() {
     createCanvas(600, 600);
-    background(0);
+    background(0, 102, 153);
     angleMode(DEGREES);
+}
+function makeArm() {
+    let alt = Math.round(rotateBy /360);
+    console.log(alt);
+    noFill();
+    stroke(0, 68, 102);
+    strokeWeight(1);
+    ellipse(150, 150 + alt, 150 / alt);
+    stroke(26, 178, 255);
+    square (50, 50, 20);
+    stroke(128, 212, 255);
+    square (100, 100, 10);
 }
 
 function draw() {
     translate(300, 300);
-    rotate(5);//come back to this
-    ellipse(150,150,150);
+    rotate(rotateBy);
+    makeArm(rotateBy);
+    rotateBy += 8;
+}
 
+function mousePressed() {
+    noLoop();
 }
