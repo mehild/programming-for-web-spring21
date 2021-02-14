3ca965856ec44727ec924ec13b25ddb8d33c85c9
mehild <mhild@mcad.edu>
2021-02-14 18:29:03 -0500

    labels

--- week 4/exercises/4.1/dom.js
+++ week 4/exercises/4.1/dom.js
@@ -1,13 +1,17 @@
 const myEmptyDiv = document.querySelector('#myEmptyDiv');
 
+//heading element
 const heading = document.createElement('h1');
 
 heading.innerHTML = 'Llamas are the camel’s hippie cousins. They belong to a group of animals called camelids that also includes alpacas. All camelids spit or stick out their tongue when they are annoyed.';
 
+//dom
 heading.addEventListener('click', handleHeadingClick); 
 
+//etc
 myEmptyDiv.appendChild(heading);
 
+//background
 function handleHeadingClick () {
     document.body.style.backgroundColor = '#B28CFE';
   }
