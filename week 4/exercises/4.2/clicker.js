abfa1c499919e02202880e81ad346562ef728b56
mehild <mhild@mcad.edu>
2021-02-14 19:01:47 -0500

    fix rects

--- week 4/exercises/4.2/clicker.js
+++ week 4/exercises/4.2/clicker.js
@@ -1,4 +1,29 @@
+let recX = 0;
+const rectWidth = 75;
+let clickCount = 0;
 function setup () {
     createCanvas(500, 500);
+    rectY = random(height - rectHeight);
+}
+
+function draw () {
     background('#DAF7A6');
+    drawShape();
+    rectX++;
+    if( rectX > width) {
+        noLoop();
+        text('Your score was ' + clickCount, 100, 300);
+    }
+}
+
+function mousePressed() {
+    if ((mouseX >= rectX && mouseX <= rectX + rectWidth) && (mouseY >= 0 && mouseY <= 75)) {
+        clickCount++;
+        console.log('hit', clickCount);
+    }
+}
+
+function drawShape() {
+    fill('white');
+    rect(rectX, rectY, rectWidth, rectHeight);
 }
