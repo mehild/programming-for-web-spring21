d13a3589fc15814e42f16cfaaae9cd575254b186
mehild <mhild@mcad.edu>
2021-02-20 17:32:34 -0500

    js set up

--- week 5/memory/memory.js
+++ week 5/memory/memory.js
@@ -1,16 +1,34 @@
-function setup() {
-    createCanvas(200, 200);
-    noLoop();
+const circleDiameter = 200;
+
+let startingX = 250;
+let startingY = 250;
+let myCircles = [];
+let startingId = 0;
+function setup () {
+    createCanvas(windowWidth, windowHeight);
+    background('#66ccff');
+    fill ('#cceeff');
+    stroke ('white');
+    
+    for (let k = 0; k < 3; k++) {
+        for (let i = 0; i < 4; i++) {
+            ellipse(startingX, startingY, circleDiameter);
+            myCircles.push({ x: startingX, y: startingY, id: startingId });
+            startingX += 230;
+            startingId++;
+        }
+        startingY += 230;
+        startingX = 250;
+    }
+    
+    console.log(myCircles);
 }
 
-function createTile() {
-    translate(0, 0);
-    fill('#cceeff');
-    circle(200, 200);
-    stroke('#558000');
-    strokeWeight('10');
-}
-
-function draw() {
-    createTile();
+function mousePressed() {
+    for (let j = 0; j < myCircles.length; j++) {
+        let distance = dist(mouseX, mouseY, myCircles[j].x, myCircles[j].y);
+        if (distance < circleDiameter / 2) {
+            console.log('circle has been click', myCircles[j].id);
+        }
+    }
 }
