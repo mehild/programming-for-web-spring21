b02f2d81ef6956da845b43281e47c92d80c5d8be
mehild <mhild@mcad.edu>
2021-02-22 01:30:34 -0500

    comparison check
    image upload

--- week 5/memory/memory.js
+++ week 5/memory/memory.js
@@ -1,3 +1,35 @@
+const DOWN = 'down';
+const UP = 'up';
+let startingX = 200;
+let startingY = 200;
+let cards = [];
+const gameState = {
+    totalPairs: 6,
+    flippedCards: [],
+    numMatched: 0,
+    attempts: 0,
+    waiting: false,
+};
+
+let cardfaceArray = [];
+
+let cardback;
+
+function preload() {
+    cardback = loadImage('img/cardback.png');
+    cardfaceArray = [
+        loadImage('img/flake1.jpg'),
+        loadImage('img/flake2.jpg'),
+        loadImage('img/flake3.jpg'),
+        loadImage('img/flake4.jpg'),
+        loadImage('img/flake5.jpg'),
+        loadImage('img/flake6.jpg'),
+    ];
+}
+
+
+
+
 function setup() {
     createCanvas(200, 200);
     noLoop();
