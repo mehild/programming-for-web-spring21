90f729a96957d2d74a4ef80fdad2cfdae7c4c7e7
mehild <mhild@mcad.edu>
2021-03-01 20:06:44 -0500

    llama facts

---
+++ week 6/6a.js
@@ -0,0 +1,14 @@
+
+
+{ data: {
+    llamaFact1: "Llamas are social animals."
+    llamaFact2: "Life expectancy of a llama is 20-25 years."
+    llamaFact2: "Baby llamas are called crias."
+    llamaFact4: "Llamas are often used as guard animals."
+    llamaFact5: "Llamas are native to South America."
+    llamaFact6: "Llamas can hum."
+    llamaFact7: "Llama poop is a source of fuel for fires."
+    llamaFact8: "Llama species is glama."
+    llamaFact9: "Adult llamas can weigh between 280-340 lbs."
+    llamaFact10: "Llamas eat grass."
+ } }
