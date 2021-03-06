6b0865656e4030c108d5d4770a7865e0ecd772f1
mehild <mhild@mcad.edu>
2021-03-05 22:00:45 -0500

    titles

--- week 7/7.1/script.js
+++ week 7/7.1/script.js
@@ -1,7 +1,9 @@
 new Vue({
     el: "diff",
     data: { 
-      llamaWiki: "https://en.wikipedia.org/wiki/Llama";
-      alpacaWiki: "https://en.wikipedia.org/wiki/Alpaca"
+      llamaWiki: "https://en.wikipedia.org/wiki/Llama",
+      llamaTitle: "llama wiki",
+      alpacaWiki: "https://en.wikipedia.org/wiki/Alpaca",
+      alpacaTitle: "alpaca title",
     }
 });
