bcc3e716bcedd5696a881f65d20be87fa28c6ffb
mehild <mhild@mcad.edu>
2021-04-12 09:22:42 -0400

    final commit

--- week 11/loop.js
+++ week 11/loop.js
@@ -1,7 +1,7 @@
 
 function setup() {
     createCanvas(displayWidth, displayHeight);
-  }
+}
   
 function draw() {
     background (171, 219, 227);
@@ -9,8 +9,8 @@ function draw() {
     noFill();
     stroke(234, 182, 118);
     for (var y = 0; y <= height; y += 8 ) {
-    for (var x = 0; x <= width; x += 15) {
-        ellipse( x + y, y, 16 - y/10.0, 16 - y/10.0);
+        for (var x = 0; x <= width; x += 15) {
+            ellipse( x + y, y, 16 - y/10.0, 16 - y/10.0);
         }
     }
-  }
+}
