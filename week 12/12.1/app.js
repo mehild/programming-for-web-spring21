6dd4983eb7cb03faa951752e55038f883e1277b5
mehild <mhild@mcad.edu>
2021-04-16 00:30:07 -0400

    error experimentation

--- week 12/12.1/app.js
+++ week 12/12.1/app.js
@@ -1,32 +1,18 @@
-//var mySound;
-
-//function preload() {
-   // mySound = loadSound('forest.wav');
-}
-
-//function setup() {
-   // cnv = createCanvas(500, 500);
-   // mySound.setVolume(0.1);
-   // mySound.play();
-}
-
-  //function draw() {
-      
-}
-
 function preload() {
     soundFormats('wav');
-    mySound = loadSound('forest');
-  }
+    mySound = loadSound('forest.wav');
+}
   
   function setup() {
-    let cnv = createCanvas();
+    cnv = createCanvas(displayWidth, displayHeight);
     cnv.mousePressed(canvasPressed);
-    background(220);
   }
+
+  function draw() {
+    background(204, 204, 255);
+      
+}
   
   function canvasPressed() {
-    // playing a sound file on a user gesture
-    // is equivalent to `userStartAudio()`
     mySound.play('forest.wav');
   }
