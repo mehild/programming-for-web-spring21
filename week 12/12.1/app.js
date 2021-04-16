c4096f09dfec78057ebca1f550acecdc2937b0a2
mehild <mhild@mcad.edu>
2021-04-16 10:35:38 -0400

    frustrated
    still won't work exhausted possibilities

--- week 12/12.1/app.js
+++ week 12/12.1/app.js
@@ -1,5 +1,7 @@
 var level; 
 var mySound;
+var circleX = 250
+var circleY = 250
 
 function preload() {
     //soundFormats('wav');
@@ -7,15 +9,24 @@ function preload() {
 }
   
   function setup() {
-    let cnv = createCanvas(displayWidth, displayHeight);
-    cnv.mousePressed(canvasPressed);
-  }
+    createCanvas(displayWidth, displayHeight);
+    mySound.setVolume(0.1);
+    mySound.play();
+    
+    //let cnv = createCanvas(displayWidth, displayHeight);
+    //cnv.mousePressed(canvasPressed);
+}
 
   function draw() {
-    background(204, 204, 255);
+    //background(204, 204, 255);
+    level = mySound.getLevel();
+    var diameterX = level * 2500
+    var diameterY = level * 2500 * random (1.5)
+
+    ellipse(circleX, circleY, diameterX, diameterY)
       
 }
   
-  function canvasPressed() {
-    mySound.play();
-  }
+  //function canvasPressed() {
+   // mySound.play();
+  //}
