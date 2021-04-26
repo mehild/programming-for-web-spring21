28fb006e6416b2655a831ce92cf7212acc67bcba
mehild <mhild@mcad.edu>
2021-04-25 23:48:12 -0400

    fixes

--- week 13/app.js
+++ week 13/app.js
@@ -1,6 +1,13 @@
 function setup() {
-    createCanvas(500 x 500);
+    createCanvas(500, 500);
+    angleMode(DEGREES);
+
+    pos = 0.0 
+    inc = 3.0
 }
 function draw() {
-    ellipse(width/2, height/2, 50, 50)
+    var mySinVal = sin(pos)
+    console.log(mySinVal)
+    ellipse(mySinVal + 100, height/2, 50, 50)
+    pos = pos + inc
 }
