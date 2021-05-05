970fa5df226e388867c907fdce71eb4d869c1250
mehild <mhild@mcad.edu>
2021-05-05 14:41:05 -0400

    Still no luck

--- week 12/12.A/app copy.js
+++ week 12/12.A/app copy.js
@@ -2,47 +2,37 @@ var sparrow;
 let sky;
 
 var sound2;
-var sound3; 
-var sound5;
+let sound3; 
+let sound5;
 
-let bird1; 
-let bird2; 
-let bird3; 
+var bird1; 
+var bird2; 
+var bird3; 
 
 function preload() {
     sparrow = loadImage("images/2.png");
     sky = loadImage("images/sky.png");
+
     bird1 = loadImage("images/4.png");
     bird2 = loadImage("images/5.png");
     bird3 = loadImage("images/3.png");
+
     soundFormats('wav');
     sound2 = loadSound('sound/sound2.wav');
     sound3 = loadSound('sound/sound2.wav');
     sound5 = loadSound('sound/sound2.wav');
 }
-
-function draw() {
-    background(sky);
-    image(sparrow, mouseX, mouseY);
-    image(cloud1, 200, 400);
-
-}
-
 function setup() {
     canvas = createCanvas(displayWidth, displayHeight);  
 }
-
-function playA () {
-    sound2.play();
+function draw() {
+    background(sky);
+    image(sparrow, mouseX, mouseY);
+    image(bird1, 200, 200);
+    image(bird2, 500, 400);
+    image(bird3, 800, 300);
 }
 
-function playB () {
-    sound3.play();
-}
-
-function playC () {
-    sound5.play();
-}
 
 //https://creative-coding.decontextualize.com/media/ while looking for resources to assist me I found a way to tether an image to the mouse.
 
