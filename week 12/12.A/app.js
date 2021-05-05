ffbc8f6d7d6cba7a0a36070ca8eb5acfa1f3dd3f
mehild <mhild@mcad.edu>
2021-05-05 11:46:30 -0400

    playing out seperate concept

--- week 12/12.A/app.js
+++ week 12/12.A/app.js
@@ -1,25 +1,3 @@
-var mySound
-let img
 
-function preload () {
-    img = loadImage('images/sky.png');//add image
-    mySound = loadSound('forest.wav');
-}
-
-function setup () {
-    const cnv = createCanvas(displayWidth, displayHeight);
-    image(img, 0, 0);
-    cnv.mouseClicked(toggleSound);
-    mySound.setVolume(1);
-}
-
-function toggleSound () {
-    if (mySound.isPlaying()) {
-        mySound.stop();
-    } 
-    else {
-        mySound.play();
-    }
-}
 
 //https://p5js.org/reference/#/libraries/p5.sound
