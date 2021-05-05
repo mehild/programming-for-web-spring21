2565fa7594c036a32e4133ebff91d69727ec716f
mehild <mhild@mcad.edu>
2021-05-05 19:25:35 -0400

    set up all

---
+++ week 11/11A/repeat1/loop.js
@@ -0,0 +1,17 @@
+
+function setup() {
+    createCanvas(displayWidth, displayHeight);
+}
+  
+function draw() {
+    background (171, 219, 227);
+    strokeWeight(1);
+    noFill();
+    stroke(234, 182, 118);
+    for (var y = 0; y <= height; y += 8 ) {
+        for (var x = 0; x <= width; x += 15) {
+            ellipse( x + y, y, 16 - y/10.0, 16 - y/10.0);
+        }
+    }
+}
+//created a variation on ex 4-13 from our text. orginally tried with arc but had no luck. computer overheated a lot during experimentations.
