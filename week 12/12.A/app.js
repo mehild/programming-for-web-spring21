93dd46b740c823fc3942851947603e2d353022bd
mehild <mhild@mcad.edu>
2021-05-05 12:02:30 -0400

    Bird in Flight

--- week 12/12.A/app.js
+++ week 12/12.A/app.js
@@ -1,3 +1,16 @@
+var sparrow;
+let sky;
 
+function preload() {
+  sparrow = loadImage("images/2.png");
+  sky = loadImage("images/sky.png");
+}
+function setup() {
+  createCanvas(displayWidth, displayHeight);
+}
+function draw() {
+  background(sky);
+  image(sparrow, mouseX, mouseY);
+}
 
 //https://p5js.org/reference/#/libraries/p5.sound
