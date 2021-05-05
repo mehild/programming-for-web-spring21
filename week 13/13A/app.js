e6908d16b8351acf7585d6bbbdb771e96a0e67d9
mehild <mhild@mcad.edu>
2021-05-05 17:22:24 -0400

    reorganizing / renaming files

--- week 13/13A/app.js
+++ week 13/13A/app.js
@@ -1,26 +1,46 @@
-var pos
-var inc
-let img
+var ballx = 300;
+var bally = 600;
+var ballwidth = 50;
+var ballheight = 50;
 
-function preload() {
-    img = loadImage('ball.png');
-}
+var balldirectionx = 1;
+var balldirectiony = -1;
+var ballspeed = 5;
+
+let bg;
 
 function setup() {
-    createCanvas(900, 900);
-    angleMode(DEGREES);
-
-    pos = 0.0; 
-    inc = 0.3;
+    createCanvas(windowWidth, windowHeight);
+    bg = loadImage('court.jpeg'); //background
 }
 
 function draw() {
-    clear();
-    translate(width/10, height/10);
+   background(bg);
+   noStroke(); 
+   fill(173,255,47); //tennis ball color
 
-    var mySinVal = sin(pos)
-    console.log(mySinVal);
-    
-    amplified = mySinVal * 1000
-    image(img, 1, amplified);    
+   ballx = ballx + (balldirectionx*ballspeed);
+   bally = bally + (balldirectiony*ballspeed);
+
+   ellipse(ballx, bally, ballwidth, ballheight); //tennis ball
+
+   if(ballx < 0) {
+       balldirectionx = balldirectionx*-1; 
+   }
+
+   if(ballx > width) {
+    balldirectionx = balldirectionx*-1; 
+   }
+
+   if(bally < 0) {
+    balldirectiony = balldirectiony*-1;
+   }
+
+   if(bally > height) {
+    balldirectiony = balldirectiony*-1;
+   }
 }
+
+//used https://youtu.be/Kp070rI_G48 as a guide... somewhat helpful.
+
+//tried to get the ellipse to be an image of a tennis ball... was unsuccessful.
