85830fa20b5360a5751140b5ddbb731f01d090f1
mehild <mhild@mcad.edu>
2021-05-05 20:39:13 -0400

    final push

--- week 11/11A/repeat1/loop.js
+++ week 11/11A/repeat1/loop.js
@@ -1,14 +1,14 @@
 
 function setup() {
     createCanvas(600,600);
-    background(128, 128, 0);
+    background(0, 128, 0)
 }
   
 function draw() {
     if (mouseIsPressed) {
         fill (0, 255, 0);
     } else {
-        fill(0, 128, 0);
+        fill(128, 128, 0);
     }
     ellipse(mouseX, mouseY, 100, 100);
 }
