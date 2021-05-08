34d5fe8ecfef06686b7923ccfb1b50d4c8208845
mehild <mhild@mcad.edu>
2021-05-07 22:57:58 -0400

    preload images

--- final/app.js
+++ final/app.js
@@ -1,3 +1,45 @@
+const DOWN = 'down';
+const UP = 'up';
+let startingX = 100;
+let startingY = 100;
+let cards = [];
+const gameState = {
+  totalPairs: 5,
+  flippedCards: [],
+  numMatched: 0,
+  attempts: 0,
+  waiting: false
+};
+let cardBack;
+let cardfaceArray = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+
+function draw () {
+}
+
+// preload the images
+function preload() {
+  cardBack = loadImage('images/back.png');
+  cardfaceArray = [
+    loadImage('images/a.png'),
+    loadImage('images/c.png'),
+    loadImage('images/direct.png'),
+    loadImage('images/eye.png'),
+    loadImage('images/i.png'),
+    loadImage('images/magic.png'),
+    loadImage('images/p.png'),
+    loadImage('images/pen.png'),
+    loadImage('images/select.png'),
+    loadImage('images/sissors.png'),
+    loadImage('images/v.png'),
+    loadImage('images/y.png'),
+  ]
+}
+
+
 
 function shuffleArray (array) {
     let counter = array.length;
@@ -13,3 +55,4 @@ function shuffleArray (array) {
     }
     return array;
   }
+}
