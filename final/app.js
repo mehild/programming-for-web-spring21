2a1493a713b5fad6c9390fb298b61a9127b8b81f
mehild <mhild@mcad.edu>
2021-05-07 23:53:29 -0400

    did hit

--- final/app.js
+++ final/app.js
@@ -1,24 +1,38 @@
 //following week 5 tutorials
+let myCard; 
 
 function setup() {
     //createCanvas(windowWidth, windowHeight);
     createCanvas(800, 600); //tutorial
     background(230, 230, 250);
-    let myCard = new Card();
+    myCard = new Card();
+}
+
+function mousePressed() {
+    console.log(myCard.didhit(mouseX, mouseY));
+
 }
 
 class Card {
     constructor () {
         this.x = 100;
         this.y = 100;
-        this.width = 80;
-        this.height = 80;
+        this.width = 80; //key
+        this.height = 80; //key
         this.show();
     }
 
     show () {
-        fill(0);
+        fill(0); //black
         rect(this.x, this.y, this.width, this.height, 10);
     }
+
+    didHit (mouseX, mouseY) {
+        if (mouseX >= this.x && mouseX <= this.x + this.width && mouseY >= this.y && mouseY <= this.y + this.height){
+            return true;
+        }
+
+    }
+
 }
 
