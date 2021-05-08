b86ba938ee410657fa2337ad4c3bb09ef7547c9b
mehild <mhild@mcad.edu>
2021-05-07 23:34:01 -0400

    tutorial not generating anything

--- final/app.js
+++ final/app.js
@@ -1,28 +1,22 @@
 //following week 5 tutorials
-class Card
-
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+    //createCanvas(windowWidth, windowHeight);
+    createCanvas(800, 600); //tutorial
+    background(0);
+    let myCard = new Card();
+    myCard.show();
 
-function draw () {
-}
+class Card {
+    constructor () {
+        this.x = 100;
+        this.y = 100;
+        this.width = 80;
+        this.height = 100;
 
-// preload the images
-function preload() {
-  cardBack = loadImage('images/back.png');
-  cardfaceArray = [
-    loadImage('images/a.png'),
-    loadImage('images/c.png'),
-    loadImage('images/direct.png'),
-    loadImage('images/eye.png'),
-    loadImage('images/i.png'),
-    loadImage('images/magic.png'),
-    loadImage('images/p.png'),
-    loadImage('images/pen.png'),
-    loadImage('images/select.png'),
-    loadImage('images/sissors.png'),
-    loadImage('images/v.png'),
-    loadImage('images/y.png'),
-  ]
+    }
+    show () {
+        fill(#ff0000);
+        rect(this.x, this.y, this.width, this.height, 10);
+    }
 }
