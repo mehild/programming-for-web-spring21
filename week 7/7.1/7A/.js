d87f0df2d60e6d3ea33dbb7aa5dd72bff18e7955
mehild <mhild@mcad.edu>
2021-05-10 13:54:37 -0400

    Add list data

---
+++ week 7/7.1/7A/.js
@@ -0,0 +1,15 @@
+  {
+    name: "Echinopsis huascha",
+    primary color: "Green / Lime",
+    bloom color: "Red Bloom",
+    special characteristic: "Showy Blooms, Hard to Kill",
+    image: "1.png",
+  }
+
+  {
+    name: "Frailea knippeliana",
+    primary color: "Green / Lime",
+    bloom color: "Yellow Bloom",
+    special characteristic: "Showy Blooms, Slow Grower, Lots of Offsets",
+    image: "2.png",
+  }
