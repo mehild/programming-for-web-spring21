27e327502e93f295570b64bc9e0a13e7768385b2
mehild <mhild@mcad.edu>
2021-05-10 14:01:06 -0400

    rearrange files
    complete list

---
+++ week 7/7A/cacti.js
@@ -0,0 +1,39 @@
+  {
+    name: "Echinopsis huascha",
+    primary color: "Green / Lime",
+    bloom color: "Red Bloom",
+    special characteristic: "Showy Blooms, Hard to Kill",
+    image: "1.png",
+  }
+
+  {
+    name: "Frailea knippeliana",
+    primary color: "Green / Lime",
+    bloom color: "Yellow Bloom",
+    special characteristic: "Showy Blooms, Slow Grower, Lots of Offsets",
+    image: "2.png",
+  }
+  
+  {
+    name: "Parodia haselbergii",
+    primary color: "Green / Lime",
+    bloom color: "Red Bloom",
+    special characteristic: "Showy Blooms, Slow Grower",
+    image: "3.png",
+  }
+
+  {
+    name: "Parodia werneri",
+    primary color: "Green / Lime",
+    bloom color: "Pink Bloom",
+    special characteristic: "Showy Blooms, Hard to Kill",
+    image: "4.png",
+  }
+
+  {
+    name: "Parodia procera",
+    primary color: "Green / Lime",
+    bloom color: "Orange Bloom, Yellow Bloom",
+    special characteristic: "Showy Blooms, Hard to Kill, Slow Grower",
+    image: "5.png",
+  }
