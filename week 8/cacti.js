bb6299bb041888785ef8dbf2de52bc88c16c84f5
mehild <mhild@mcad.edu>
2021-05-10 23:07:14 -0400

    organizing files

---
+++ week 8/cacti.js
@@ -0,0 +1,49 @@
+//https://mountaincrestgardens.com/ information, images
+  
+  var myVue = new Vue({
+    el: "#app",
+    data: {
+      cacti: [
+  
+        {
+          name: 'Echinopsis huascha',
+          primary: 'Green / Lime',
+          bloom: 'Red Bloom',
+          special: 'Showy Blooms, Hard to Kill',
+          image: 'img/1.png',
+        },
+
+        {
+          name: 'Frailea knippeliana',
+          primary: 'Green / Lime',
+          bloom: 'Yellow Bloom',
+          special: 'Showy Blooms, Slow Grower, Lots of Offsets',
+          image: 'img/2.png',
+        },
+        
+        {
+          name: 'Parodia haselbergii',
+          primary: 'Green / Lime',
+          bloom: 'Red Bloom',
+          special: 'Showy Blooms, Slow Grower',
+          image: 'img/3.png',
+        },
+
+        {
+          name: 'Parodia werneri',
+          primary: 'Green / Lime',
+          bloom: 'Pink Bloom',
+          special: 'Showy Blooms, Hard to Kill',
+          image: 'img/4.png',
+        },
+
+        {
+          name: 'Parodia procera',
+          primary: 'Green / Lime',
+          bloom: 'Orange Bloom, Yellow Bloom',
+          special: 'Showy Blooms, Hard to Kill, Slow Grower',
+          image: 'img/5.png',
+        },
+      ]
+    }
+  });
